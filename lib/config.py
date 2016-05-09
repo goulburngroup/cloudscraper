150cef342804c6655950586c515c6798a34587ed
Brendan Jurd <direvus@gmail.com>
2016-05-09 16:22:22 +1000

    Clean up and simply CLI script.
    Remove options that are not currently supported -- we are targeting a
    simple scrape-and-store strategy, where we collect info from the API and
    then put it in a database.  Reporting is a story for another day.
    
    Make lib.config.Config a subclass of RawConfigParser, this reduces the
    amount of code needed to get things done.

--- lib/config.py
+++ lib/config.py
@@ -9,51 +9,18 @@ Authors:
     Alex Ferrara <alex@receptiveit.com.au>
     Brendan Jurd <direvus@gmail.com>
 """
-import ConfigParser
+from ConfigParser import RawConfigParser
 
 
-class Config:
+class Config(RawConfigParser):
     def __init__(self, config_file):
-        self.config = ConfigParser.RawConfigParser()
-        self.config.read(config_file)
-
-        self.database = {'type': self.config.get('database', 'type')}
-
-        if self.database['type'] != "none":
-            self.database.update({'host': self.config.get('database', 'host'),
-                                  'database': self.config.get('database',
-                                                         'database'),
-                                  'username': self.config.get('database',
-                                                         'username'),
-                                  'password': self.config.get('database',
-                                                         'password')})
-
-        self.email = dict(self.config.items('email'))
-
-    def get(self, section, key):
-        return self.config.get(section, key)
+        super(Config, self).__init__()
+        self.read(config_file)
 
     def get_db(self):
-        """Return database config"""
-        return self.database
+        """Return database config."""
+        return dict(self.items('database'))
 
     def get_email(self):
         """Return email config"""
-        return self.email
-
-    def get_network(self):
-        """Return network config"""
-        return self.network
-
-    def get_node_settings(self, node_name):
-        """Return network quota config"""
-        if not self.config.has_section(node_name):
-            node_name = 'net_default'
-
-        return dict(self.config.items(node_name))
-
-    def set_network(self, network):
-        """Set a single network"""
-        self.network['name'] = network
-        self.network['recurse'] = False
-
+        return dict(self.items('email'))
